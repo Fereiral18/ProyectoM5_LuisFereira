d12f21473ddd7a581033110a186d54476ca83aa6
Luis fereira <fereiraluis18051997@gmail.com>
2026-05-24 15:13:46 -0300

    fix: se creo el context de authentication, para saber cuando el user tengo sesion abierta y para saber que usuario tienen la sesion abierta

---
+++ src/context/auth.context.ts
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+import type { AuthUser } from "../types/auth.type";
+
+
+export interface AuthContextValue {
+  user: AuthUser | null;
+  loading: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  signup: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextValue | null>(null);
