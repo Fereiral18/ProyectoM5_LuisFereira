7e406c19e47d525221d5992893163fca4b58aafb
Luis fereira <fereiraluis18051997@gmail.com>
2026-05-25 01:02:22 -0300

    fix: se agrego el debounce para el renderizado en tiempo real de la lista de productos en base a la info que se suministra en el input de busqueda de articulos

--- src/providers/ProductsProvider.tsx
+++ src/providers/ProductsProvider.tsx
@@ -1,13 +1,18 @@
 // src/providers/ProductsProvider.tsx
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getProducts } from "../services/product.service";
 import type { Product } from "../types/products.type";
 import { ProductsContext } from "../context/products.context";
+import { useDebounce } from "../hooks/useDebaunce";
 
 export function ProductsProvider({ children }: { children: React.ReactNode }) {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+
+  //* DEBOUNCE
+  const debouncedSearch = useDebounce(search, 500);
 
   async function loadProducts() {
     try {
@@ -27,12 +32,33 @@ export function ProductsProvider({ children }: { children: React.ReactNode }) {
     loadProducts();
   }, []);
 
+   //* FILTER PRODUCTS
+  const filteredProducts = useMemo(() => {
+    if (!debouncedSearch.trim()) {
+      return products;
+    }
+
+    return products.filter((product) =>
+      product.name
+        .toLowerCase()
+        .includes(
+          debouncedSearch.toLowerCase()
+        )
+    );
+  }, [products, debouncedSearch]);
+
   return (
     <ProductsContext.Provider
       value={{
-        products,
+        products: filteredProducts,
+
         loading,
+
         loadProducts,
+
+        search,
+
+        setSearch,
       }}
     >
       {children}
